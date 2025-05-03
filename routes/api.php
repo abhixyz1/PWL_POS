fbcfd5f2d41b0a401f9dacb214f07d42cf6f5d17
abhixyz1 <abhinayazrf@gmail.com>
2025-05-03 15:48:40 +0700

    Jobsheet 10 - Praktikum 03

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\RegisterController;
 use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\LogoutController; // Pastikan ini di-use
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,15 +10,19 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
 */
 
-Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
-Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
-Route::middleware('auth:api')->get('/user', function (Request $request){
-    return $request->user();
+Route::post('/register', RegisterController::class)->name('register');
+Route::post('/login', LoginController::class)->name('login');
+
+Route::middleware('auth:api')->group(function () {
+
+    // Rute untuk mendapatkan data user yang sedang login
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    // Rute untuk logout
+    Route::post('/logout', LogoutController::class)->name('logout');
+
 });
